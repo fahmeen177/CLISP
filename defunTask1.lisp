3254fdfbf9497ad993ea9e01f5774dd4e3fa8636
Ashstarfire <fahmeenmazhar@gmail.com>
2019-03-17 22:10:33 +0500

    Update defunTask1.lisp

--- defunTask1.lisp
+++ defunTask1.lisp
@@ -1,15 +1,15 @@
 ; Addition Function
-(defun add(f m) (+ f m)
+(defun add(f m) (+ f m))
 (add 10 20)
 
 ; Subtraction Function
-(defun sub(f m) (- f m)
+(defun sub(f m) (- f m))
 (sub 10 20)
 
 ; Multiplication Function
-(defun mul(f m) (* f m)
+(defun mul(f m) (* f m))
 (mul 10 20)
 
 ; Division Function
-(div mul(f m) (/ f m)
+(div mul(f m) (/ f m))
 (div 10 20)
